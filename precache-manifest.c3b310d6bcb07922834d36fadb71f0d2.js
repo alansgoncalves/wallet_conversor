5e28eb30959ee1b5ddd77c32136e1f9572df5f6b
Alan Gonçalves <vekio.soft@gmail.com>
2021-10-19 12:29:34 -0300

    Updates

---
+++ precache-manifest.c3b310d6bcb07922834d36fadb71f0d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95f4aabd5cdc9f79c4db7efdfacea6ac",
+    "url": "/wallet_conversor/index.html"
+  },
+  {
+    "revision": "19dba37ca0a428cbc8a9",
+    "url": "/wallet_conversor/static/css/main.74376778.chunk.css"
+  },
+  {
+    "revision": "9bbcb947af37f1f2d744",
+    "url": "/wallet_conversor/static/js/2.816ef285.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/wallet_conversor/static/js/2.816ef285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19dba37ca0a428cbc8a9",
+    "url": "/wallet_conversor/static/js/main.d3845367.chunk.js"
+  },
+  {
+    "revision": "701e026488321426da3a",
+    "url": "/wallet_conversor/static/js/runtime-main.bb052a87.js"
+  },
+  {
+    "revision": "3b8cfe3576371f3678385f441d4dadf5",
+    "url": "/wallet_conversor/static/media/wallet.3b8cfe35.png"
+  }
+]);
